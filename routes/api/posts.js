bb7d7018a0301f39da1eb892e1b999b92fcba8c3
Gulshan-p <78453319+Gulshan-p@users.noreply.github.com>
2021-04-01 11:24:19 -0700

    create post and post validations

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,39 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
+//load Posts and Profile model
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+// Load Validation
+const validatePostInput = require("../../validation/post");
 //test
-router.get('/test', (req,res) => res.json({msg: 'post works.'}));
+//router.get('/test', (req,res) => res.json({msg: 'post works.'}));
 //export
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+    //save post
+    newPost.save().then(post => res.json(post));
+  }
+);
+
 module.exports = router;
---
+++ validation/post.js
@@ -0,0 +1,18 @@
+const validator = require('validator');
+const isEmpty = require('./is-empty');
+//add module.export and call the function
+module.exports = function validatePostInput(data){
+    let errors = {};
+//add validations
+//for text field
+if(!validator.isLength(data.text, {min:15, max:300})){
+  errors.text = 'text must be between 15 and 300 characters';
+   }
+   if(isEmpty(data.text)){
+     errors.text = 'Text field is required';
+   }
+   return{
+    errors,
+    isValid: isEmpty(errors)
+  }
+}
