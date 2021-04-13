3ef36799b9019fe4b44aadeae6e6d1b90e72e254
Gulshan-p <78453319+Gulshan-p@users.noreply.github.com>
2021-04-13 16:26:35 -0700

    added footer, navbar and landing components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
+import React, { Component } from 'react'
 import './App.css';
-function App() {
-  return (
-    <div className="App">
-      <h1>my React App</h1>
+import Navbar from './components/Layout/Navbar';
+import Landing from './components/Layout/Landing';
+import Footer from './components/Layout/Footer';
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Navbar />
+      <Landing />
+      <Footer />
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+
+
