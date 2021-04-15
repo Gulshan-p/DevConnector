e97bed7b4fe9afefdcb7fc4c51b8fc9f7ea9628f
Gulshan-p <78453319+Gulshan-p@users.noreply.github.com>
2021-04-15 09:17:28 -0700

    added(navbar, landing, footer) layout and setup auth folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Layout/Navbar';
 import Landing from './components/Layout/Landing';
 import Footer from './components/Layout/Footer';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
+      <Router>
+        <div className="App">
+        <Navbar />
+        <Route exact path = "/" component = {Landing} />
+        <Route exact path = "/register" component = {Register} />
+        <Route exact path = "/login" component = {Login} />
+        <Footer />
+        </div>
+      </Router>
     )
   }
 }
