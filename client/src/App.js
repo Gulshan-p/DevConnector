e7250cc3d6d95030fb32a4fe63d8784bd1fb6aa8
Gulshan-p <78453319+Gulshan-p@users.noreply.github.com>
2021-04-17 16:52:14 -0700

    created store(added redux, react-redux and redux-thunk)

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import './App.css';
 import Navbar from './components/Layout/Navbar';
 import Landing from './components/Layout/Landing';
 import Footer from './components/Layout/Footer';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import store from './store';
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-        <Navbar />
-        <Route exact path = "/" component = {Landing} />
-        <Route exact path = "/register" component = {Register} />
-        <Route exact path = "/login" component = {Login} />
-        <Footer />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+          <Navbar />
+          <Route exact path = "/" component = {Landing} />
+          <Route exact path = "/register" component = {Register} />
+          <Route exact path = "/login" component = {Login} />
+          <Footer />
+          </div>
+        </Router>
+      </Provider>
     )
   }
 }
